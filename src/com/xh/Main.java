c87d41bf03048430d565de22c6d44d0d5c582f19
liuhuiiang <825378291@qq.com>
2018-07-29 23:03:32 +0800

    提哪集actionParas对象,已经绑定id

--- src/com/xh/Main.java
+++ src/com/xh/Main.java
@@ -2,11 +2,15 @@ package com.xh;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.xh.xml.FindById;
 
 public class Main extends AnAction {
+    private ActionParas paras;
 
     @Override
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
+        paras=new ActionParas(e);
+        new FindById(paras);
     }
 }
