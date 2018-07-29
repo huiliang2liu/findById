5e41bcd2f54763a930940832a2c7208df8851230
liuhuiiang <825378291@qq.com>
2018-07-29 23:04:30 +0800

    增加对话框

--- src/com/xh/dialog/Dialog.java
+++ src/com/xh/dialog/Dialog.java
@@ -1,4 +1,18 @@
-package com.xh.com.xh.dialog;
+package com.xh.dialog;
 
-public class Dialog {
+import javax.swing.*;
+
+public class Dialog extends JFrame {
+    public Dialog(JPanel panel) {
+        setDefaultCloseOperation(2);
+        getContentPane().add(panel);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public void dismiss() {
+        setVisible(false);
+        dispose();
+    }
 }
