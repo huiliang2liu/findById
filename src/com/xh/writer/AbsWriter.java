a3c345fba985595918bf3e2b4896105ee0282b25
liuhuiiang <825378291@qq.com>
2018-07-29 23:10:05 +0800

    增加写文件抽象类

--- src/com/xh/writer/AbsWriter.java
+++ src/com/xh/writer/AbsWriter.java
@@ -1,4 +1,25 @@
 package com.xh.writer;
 
-public class AbsWriter extends  {
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiFile;
+import com.xh.ActionParas;
+
+public abstract class AbsWriter extends WriteCommandAction.Simple {
+    protected ActionParas mParas;
+    protected PsiFile mFile;
+    protected PsiClass mCls;
+    protected AbsWriter(ActionParas paras, PsiFile file, PsiClass cls) {
+        super(paras.mProject, new PsiFile[0]);
+        mParas=paras;
+        mFile=file;
+        mCls=cls;
+    }
+
+    @Override
+    protected final void run() throws Throwable {
+        writer();
+    }
+
+    protected abstract void writer();
 }
