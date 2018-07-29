3d9d8197b29761e7630e71039c592a7f61ea6bd8
liuhuiiang <825378291@qq.com>
2018-07-29 23:13:23 +0800

    增加字段类

--- src/com/xh/xml/FieldEntity.java
+++ src/com/xh/xml/FieldEntity.java
@@ -1,10 +1,10 @@
-package com.xh.com.xh.xml;
+package com.xh.xml;
 
-import com.intellij.idea.StartupUtil;
+import com.xh.utils.Constant;
 import com.xh.utils.StringUtils;
 
 public class FieldEntity {
-    public boolean use = false;//是否可用
+    public boolean use = true;//是否可用
     public boolean click = false;//是否可点击
     public boolean list = false;//是否是列表
     public String id;//布局文件中的id名
@@ -13,17 +13,22 @@ public class FieldEntity {
     public String fieldName;//字段名
 
     public FieldEntity(String fullName, String id) {
-        this.fullName = fullName;
         this.id = id;
         String packages[] = fullName.split("\\.");
         name = packages[packages.length - 1];
+        if(packages.length==1){
+         if(Constant.paths.containsKey(fullName))
+             this.fullName=Constant.paths.get(fullName);
+         else
+             this.fullName="android.widget."+fullName;
+        }
         use = true;
         String splits[] = id.split("_");
         StringBuffer sb = new StringBuffer("m");
         for (String string : splits) {
             sb.append(StringUtils.firstToUpperCase(string));
         }
-        fullName = sb.toString();
+        fieldName = sb.toString();
     }
 
 }
